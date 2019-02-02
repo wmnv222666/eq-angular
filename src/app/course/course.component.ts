300bc83803dde15166c46c4269e0f61faa11dbde
Yui Hang Ng <yng@dawsoncollege.qc.ca>
2019-02-02 12:30:00 -0500

    add pojo to course

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -13,6 +13,19 @@ import { Component } from "@angular/core";
         Max students: {{ size }} Total hours: {{ hours }} Total minutes:
         {{ hours * 60 }}
       </div>
+
+      <div>Course Object</div>
+      <div>Course name: {{ course1.name }}</div>
+      <div>Instructor name: {{ course1.instructor }}</div>
+      <div>
+        Max students: {{ course1.size }} Total hours: {{ course1.hours }} Total
+        minutes:
+        {{ course1.hours * 60 }}
+      </div>
+      <div>
+        Favorite Movie: {{ course1.movie["favorite movie"] }}
+        <img [src]="course1.movie.image" />
+      </div>
     </div>
   `
 })
@@ -26,6 +39,10 @@ export class CourseComponent {
     name: "angular",
     size: 6,
     instructor: "peter",
-    hours: 3
+    hours: 3,
+    movie: {
+      "favorite movie": "Shawshank Redemption",
+      image: "https://via.placeholder.com/100"
+    }
   };
 }
